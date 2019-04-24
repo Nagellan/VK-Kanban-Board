1d2abd1de57512814feef125f8b2c2f051e94de8
Nagellan <31626879+Nagellan@users.noreply.github.com>
2019-04-24 20:51:56 +0300

    Add functionality to add & save the card

---
+++ script.js
@@ -0,0 +1,42 @@
+function addCard(btn) {
+    var cardsWrapper = btn.parentElement.getElementsByClassName('cards-wrapper')[0],
+        emptyCard = getElementFromTemplate(document.getElementById('empty-card'));
+    
+    cardsWrapper.appendChild(emptyCard)
+
+    replaceBlock(btn, getElementFromTemplate(document.getElementById("save-cancel-card")));
+    emptyCard.firstElementChild.focus();
+}
+
+function saveCard(btn) {
+    var card = getGrandParent(btn).getElementsByClassName('new')[0],
+        cardInput = card.firstElementChild,
+        addCardBtn = getElementFromTemplate(document.getElementById("add-card"));
+
+    if (cardInput.value.replace(/\s/g, '') != "") {
+        cardInput.readOnly = true;
+        card.classList.remove('new');
+        replaceBlock(btn.parentElement, addCardBtn);
+    }
+    else {
+        cardInput.value = "";
+        cardInput.focus();
+    }
+}
+
+function replaceBlock(block1, block2) {
+    block1.parentNode.replaceChild(block2, block1);
+}
+
+function getGrandParent(el) {
+    return el.parentElement.parentElement;
+}
+
+/**
+ * Transform given template block into the element.
+ * @param {HTMLElement} template - Template block.
+ * @returns {HTMLElement} Block inside the template.
+ */
+function getElementFromTemplate(template) {
+    return template.content.children.item(0).cloneNode(true);
+}
