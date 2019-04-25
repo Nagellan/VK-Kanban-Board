a9bea3cea3376064fe7ed88e0d9375c07318a9b5
Nagellan <31626879+Nagellan@users.noreply.github.com>
2019-04-25 15:17:39 +0300

    Add JSDocs

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/**
+ * Create a new empty card.
+ * @param {HTMLElement} btn - Add card button which called the function.
+ * @returns {void} Nothing, just perform necessary action(s).
+ */
 function addCard(btn) {
     var cardsWrapper = btn.parentElement.getElementsByClassName('cards-wrapper')[0],
         emptyCard = getElementFromTemplate(document.getElementById('empty-card'));
@@ -9,6 +14,11 @@ function addCard(btn) {
     emptyCard.firstElementChild.focus();
 }
 
+/**
+ * Save the data inside the newly created card, complete creation.
+ * @param {HTMLElement} btn - Save button which called the function.
+ * @returns {void} Nothing, just perform necessary action(s).
+ */
 function saveCard(btn) {
     var card = getGrandParent(btn).getElementsByClassName('card new')[0],
         cardInput = card.firstElementChild,
@@ -27,6 +37,12 @@ function saveCard(btn) {
     }
 }
 
+/**
+ * Cancel the creation of a card.
+ * @param {HTMLElement} btn - Cancel button which called the function.
+ * @param {Event} e - Escape ley handle event called the function, if exists.
+ * @returns {void} Nothing, just modify the element(s).
+ */
 function cancelCard(btn, e=null) {
     if (e != null && e.key == 'Escape')
         btn = getGrandParent(btn).parentElement.getElementsByClassName('cancel-btn')[0];
@@ -41,6 +57,11 @@ function cancelCard(btn, e=null) {
     }
 }
 
+/**
+ * Create a new empty column.
+ * @param {HTMLElement} btn - Add column button which called the function.
+ * @returns {void} Nothing, just perform necessary action(s).
+ */
 function addColumn(btn) {
     if (document.getElementsByClassName('column new').length == 0) {
         var emptyColumn = getElementFromTemplate(document.getElementById('empty-column'));
@@ -50,6 +71,11 @@ function addColumn(btn) {
     }
 }
 
+/**
+ * Save the data inside the newly created column, complete creation.
+ * @param {HTMLElement} btn - Save button which called the function.
+ * @returns {void} Nothing, just perform necessary action(s).
+ */
 function saveColumn(btn) {
     var column = document.getElementsByClassName('column new')[0],
         colTitle = column.getElementsByClassName('col-title')[0],
@@ -67,7 +93,13 @@ function saveColumn(btn) {
     }
 }
 
-function cancelColumn(btn, e) {
+/**
+ * Cancel the creation of a column.
+ * @param {HTMLElement} btn - Cancel button which called the function.
+ * @param {Event} e - Escape ley handle event called the function, if exists.
+ * @returns {void} Nothing, just modify the element(s).
+ */
+function cancelColumn(btn, e=null) {
     if (e != null && e.key == 'Escape')
         btn = btn.parentElement.getElementsByClassName('cancel-btn')[0];
 
@@ -78,10 +110,21 @@ function cancelColumn(btn, e) {
     }
 }
 
+/**
+ * Replace one block by another.
+ * @param {HTMLElement} block1 - Block to be replaced.
+ * @param {HTMLElement} block2 - Block to replace.
+ * @returns {void} Nothing, just modify the element(s).
+ */
 function replaceBlock(block1, block2) {
     block1.parentNode.replaceChild(block2, block1);
 }
 
+/**
+ * Get block's grandparent.
+ * @param {HTMLElement} elem - Original block.
+ * @returns {HTMLElement} Block's grandparent.
+ */
 function getGrandParent(el) {
     return el.parentElement.parentElement;
 }
@@ -95,15 +138,31 @@ function getElementFromTemplate(template) {
     return template.content.children.item(0).cloneNode(true);
 }
 
+/**
+ * Provide the block's dran'n'drop movement.
+ * @param {Event} e - onDragStart event.
+ * @returns {void} Nothing, just perform necessary action(s).
+ */
 function drag(e) {
     e.dataTransfer.effectAllowed = "move";
     e.dataTransfer.setData('card-id', e.target.id);
 }
 
+/**
+ * Provide the block's dran'n'drop placement.
+ * @param {Event} e - onDragOver event.
+ * @returns {void} Nothing, just perform necessary action(s).
+ */
 function dragOver(e) {
     e.preventDefault();
 }
 
+/**
+ * Provide the block's dran'n'drop placement inside a new container.
+ * @param {Event} e - onDrop event.
+ * @param {HTMLElement} block - New container for dropped element.
+ * @returns {void} Nothing, just perform necessary action(s).
+ */
 function drop(e, block) {
     e.preventDefault();
     var cardId = e.dataTransfer.getData('card-id');
